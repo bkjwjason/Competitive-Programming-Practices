103f72d99b07b61c36f0ed9e140b473011cff6a6
Jason Baek <bkjwjason@gmail.com>
2021-10-16 22:51:24 +0800

    Solved 161021

--- Problems/Atcoder128A.cpp
+++ Problems/Atcoder128A.cpp
@@ -38,19 +38,45 @@ void solve() {
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int a[4];
-	for (int i = 0; i < 4; i++){
-		cin >> a[i];
+	int n;
+	cin >> n;
+	double g = 1; double s = 0;
+	vl v;
+	for(int i = 0; i<n; i++){
+		long long x;
+		cin >> x;
+		v.pb(x);
 	}
-	string s;
-	cin >> s;
-	int sum = 0;
-	for (int i = 0; i < sz(s); i++){
-		string x = s[i];
-		int j = stoi(x);
-		cout << j << nl;
+	int b = 0;
+	if (v[b] >= v[b+1]){
+		s = g * v[b];
+		cout << 1 << " ";
+		g = 0;
 	}
-	cout << sum << nl;
+	else{
+		cout << 0 << " ";
+	}
+	for(int i = 1; i < n-1; i++){
+		if (v[i] >= v[i+1] && g!= 0){
+			s = g * v[i];
+			cout << 1 << " ";
+			g = 0;
+		}
+		else if (v[i] <= v[i+1] && s!= 0){
+			g = s / v[i];
+			cout << 1 << " ";
+			s = 0;
+		}
+		else {
+			cout << 0 << " ";
+		}
+	}
+	if (s!=0){
+		cout << 1 << " ";
+	}
+	else{
+		cout << 0 << " ";
+	}
 	return 0;
 }
 
--- Problems/Atcoder128B.cpp
+++ Problems/Atcoder128B.cpp
@@ -30,27 +30,54 @@ const char nl = '\n';
 // Solution Starts Here
 
 void solve() {
-
-
+	int r,g,b;
+	int a[3];
+	int sum = 0;
+	cin >> r >> g >> b;
+	a[0] = r; a[1] = g; a[2] = b;
+	// Check if any same number
+	for (int i = 0; i < 2; i++){
+		if (a[i] == a[i+1]){
+			cout << a[i] << nl;
+		}
+	}
+	// Check if even number
+	for (int i = 0; i < 3; i++){
+		sum += a[i];
+	}
+	if (sum % 2 == 0){
+		cout << -1 << nl;
+	}
+	// Check if all odd
+	int cnt = 0;
+	for (int i = 0; i < 3; i++){
+		if (a[i] % 2 != 0){
+			cnt++;
+		}
+	}
+	if (cnt == 3){
+		cout << -1 << nl;
+	}
+	// Needs 1 odd and 2 even number to work
+	else 
+	{
+		int num = min({r,g,b});
+		num = num + 2;
+		cout << num + 1 << nl;
+	}		
 
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int a[4];
-	for (int i = 0; i < 4; i++){
-		cin >> a[i];
+
+	int T;
+	cin >> T;
+	while(T--) {
+		solve();
 	}
-	string s;
-	cin >> s;
-	int sum = 0;
-	for (int i = 0; i < sz(s); i++){
-		string x = s[i];
-		int j = stoi(x);
-		cout << j << nl;
-	}
-	cout << sum << nl;
+
 	return 0;
 }
 
