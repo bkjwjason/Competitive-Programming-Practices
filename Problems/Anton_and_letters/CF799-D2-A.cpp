3c49ca03954c274caf35f1c123fbb4183c813124
Jason Baek <bkjwjason@gmail.com>
2022-01-23 18:52:31 +0800

    230122

--- Problems/Anton_and_letters/CF799-D2-A.cpp
+++ Problems/Anton_and_letters/CF799-D2-A.cpp
@@ -30,45 +30,25 @@ const char nl = '\n';
 // Solution Starts Here
 
 void solve() {
-	int n, k;
-	cin >> n >> k;
-	vi v1;
-	vi v2;
-	for (int i = 0; i < n; i++){
-		int x;
-		cin >> x;
-		v1.pb(x);
-	}
-	for (int i = 0; i < n; i++){
-		int x;
-		cin >> x;
-		v2.pb(x);
-	}
-	int i = 0;
-	while (i < sz(v1)){
-		if (v1[i] <= k){
-			k += v2[i];
-			v1.erase(v1.begin() + i);
-			v2.erase(v2.begin() + i);
-			i = 0;
-		}
-		else{
-			i++;
-		}
-	}
-	cout << k << nl;
+
+
+
 }
+
+set<char> st;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	string s;
+	getline(cin, s);
 
-	int T;
-	cin >> T;
-	while(T--) {
-		solve();
+	for (int i = 1; i < sz(s); i+=3){
+		if (s[i] >= 'a' && s[i] <= 'z'){
+			st.insert(s[i]);
+		}
 	}
-
+	cout << sz(st) << nl;
 	return 0;
 }
 
